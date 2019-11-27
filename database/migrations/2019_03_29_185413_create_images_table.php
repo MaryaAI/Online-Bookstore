56c8cf05bb9f687265832b202ca497a3793edc55
MaryaAI <rahaf.m.alnajjar@gmail.com>
2019-11-27 10:30:04 +0200

    First commit Models and Controllers

---
+++ database/migrations/2019_03_29_185413_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('book_id');
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('book_id')
+                  ->references('id')
+                  ->on('books')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
